e9bfbb873c25c3e8fd838e349d25c09bd38f4cf4
studentjacktainan <100222159+studentjacktainan@users.noreply.github.com>
2024-09-05 16:23:04 +0800

    login 介面

--- src/main/java/com/example/tank_war/HelloController.java
+++ src/main/java/com/example/tank_war/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.tank_war;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,12 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void submitclicked(ActionEvent actionEvent) {
+
+    }
+
+    public void cancelclicked(ActionEvent actionEvent) {
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.tank_war {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.tank_war to javafx.fxml;
